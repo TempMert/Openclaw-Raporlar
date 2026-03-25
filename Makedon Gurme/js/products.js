03d706e7ee249c741a385fcf8e67b6c0eaa4b9dc
Lea <lea@example.com>
2026-03-25 21:42:44 +0300

    Add Makedon Gurme content

---
+++ Makedon Gurme/js/products.js
@@ -0,0 +1,57 @@
+const ALL_PRODUCTS = [
+  {
+    "title": "FERRERO ROCHER 100 GR",
+    "image": "placeholder.jpg",
+    "category": "Tümü"
+  },
+  {
+    "title": "TABASCO MİLF 60 ML",
+    "image": "placeholder.jpg",
+    "category": "Tümü"
+  },
+  {
+    "title": "SAUGHEF BARBEKÜ 550 ML",
+    "image": "placeholder.jpg",
+    "category": "Tümü"
+  },
+  {
+    "title": "HP ORJİNAL SAUCE 450 ML",
+    "image": "placeholder.jpg",
+    "category": "Tümü"
+  },
+  {
+    "title": "CALVE KÖRİ SOS 260 ML",
+    "image": "placeholder.jpg",
+    "category": "Tümü"
+  },
+  {
+    "title": "CALVE ACI SOS 240 ML",
+    "image": "placeholder.jpg",
+    "category": "Tümü"
+  },
+  {
+    "title": "HEINZ TOMATO KETCHUP 460 ML",
+    "image": "placeholder.jpg",
+    "category": "Tümü"
+  },
+  {
+    "title": "HEINZ LIGHT MAYONEZ 420 ML",
+    "image": "placeholder.jpg",
+    "category": "Tümü"
+  },
+  {
+    "title": "HEINZ CREAMY MAYONEZ 330 ML",
+    "image": "placeholder.jpg",
+    "category": "Tümü"
+  },
+  {
+    "title": "HEINZ GARLIC MAYONEZ",
+    "image": "placeholder.jpg",
+    "category": "Tümü"
+  },
+  {
+    "title": "HELLMANNS MUSTARD 235 ML",
+    "image": "placeholder.jpg",
+    "category": "Tümü"
+  }
+];
